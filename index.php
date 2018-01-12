aa7afc6ea5958bbe2e6eb99dbca30a7eba883454
Antonio Suarez <asuarez2@mail.greenriver.edu>
2018-01-12 14:50:33 -0500

    Added 2 routes

--- index.php
+++ index.php
@@ -5,13 +5,33 @@
     // create instance of base class
     $f3 = Base::instance();
 
+    // set debug level
+    $f3->set('DEBUG', 3);
+
     // define a default route
     $f3->route('GET /', function() {
             echo "<h1>Routing Demo</h1>";
         }
     );
 
+    // define a page a page1 route
+    $f3->route('GET /page1', function() {
+            echo '<h1>This is page 1</h1>';
+        }
+    );
+
+    // define a page a page1 route
+    $f3->route('GET /page1/subpage-a', function() {
+            echo '<h1>This is page 1, subpage a</h1>';
+        }
+    );
+
+    // define a page a page2 route
+    $f3->route('GET /page2', function() {
+            echo '<h1>Congraturations. This story is happy end.</h1>';
+        }
+    );
+
     // run fat free
     $f3->run();
 
-?>
